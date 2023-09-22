fdf387618bb519beee6e25c827df7fda5c5c4b25
DELL <kakabuleng@gmail.com>
2023-09-23 06:48:51 +0700

    tambah search box

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toogle class active
+// Toogle class active untuk humberger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Ketika Hamburger menu diklik
@@ -6,11 +6,34 @@ document.querySelector("#humburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik diluar searchbos untuk menghilangkan search box
+// const pencarian = document.querySelector(".search-form");
+
+// document.addEventListener("click", function (e) {
+//   if (!pencarian.contains(e.target) && !searchBox.contains(e.target)) {
+//     searchBox.classList.remove("active");
+//   }
+// });
+
 // Klik diluar sidebar untuk menghilangkan sidebar
-const hamburger = document.querySelector("#humburger-menu");
+const hm = document.querySelector("#humburger-menu");
+const sb = document.querySelector("#search-btn");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchBox.classList.remove("active");
   }
 });
