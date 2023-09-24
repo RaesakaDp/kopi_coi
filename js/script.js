7f43df7cd5b216bd682afd064b1c56044c975813
DELL <kakabuleng@gmail.com>
2023-09-24 17:07:31 +0700

    tambah shopping cart dan search

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ document.querySelector("#humburger-menu").onclick = () => {
 // Toggle class active untuk search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
+const cart = document.querySelector(".shopping-cart");
 
 document.querySelector("#search-btn").onclick = (e) => {
   searchForm.classList.toggle("active");
@@ -16,24 +17,48 @@ document.querySelector("#search-btn").onclick = (e) => {
   e.preventDefault();
 };
 
-// Klik diluar searchbos untuk menghilangkan search box
-// const pencarian = document.querySelector(".search-form");
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  cart.classList.toggle("active");
+  e.preventDefault();
+};
 
-// document.addEventListener("click", function (e) {
-//   if (!pencarian.contains(e.target) && !searchBox.contains(e.target)) {
-//     searchBox.classList.remove("active");
-//   }
-// });
-
-// Klik diluar sidebar untuk menghilangkan sidebar
+// Klik diluar elemen
 const hm = document.querySelector("#humburger-menu");
 const sb = document.querySelector("#search-btn");
+const cb = document.querySelector("#shopping-cart-btn");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchBox.classList.remove("active");
+    searchForm.classList.remove("active");
+  }
+  if (!cb.contains(e.target) && !cart.contains(e.target)) {
+    cart.classList.remove("active");
   }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailBtns = document.querySelectorAll(".item-detail-btn");
+
+itemDetailBtns.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Click Tombol close modal
+document.querySelector(".modal .close-icons").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Click diluar Modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
